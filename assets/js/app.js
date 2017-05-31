e741c6530a296c56e193770fec4f3d061a813e2b
netpoetica <kthrose@netpoetica.com>
2017-05-30 21:43:48 -0400

    add momentjs to index.html, reformat stuff with tabs, and add functions for next arrival operations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,94 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBA9b-XYLS984YH8tBzTVH4IncJogW5PoY",
+	authDomain: "train-a746c.firebaseapp.com",
+	databaseURL: "https://train-a746c.firebaseio.com",
+	projectId: "train-a746c",
+	storageBucket: "train-a746c.appspot.com",
+	messagingSenderId: "600960358572"
+};
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBA9b-XYLS984YH8tBzTVH4IncJogW5PoY",
-    authDomain: "train-a746c.firebaseapp.com",
-    databaseURL: "https://train-a746c.firebaseio.com",
-    projectId: "train-a746c",
-    storageBucket: "train-a746c.appspot.com",
-    messagingSenderId: "600960358572"
-  };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
- //declare datbase variable
-  var database = firebase.database();
+//declare datbase variable
+var database = firebase.database();
 
-  //adding click handler function to submit the data
+function getNextArrival (firstTrainTime, frequency) {
+	// http://momentjs.com/docs/#/manipulating/subtract/
+	// based on the first train time
+	// the frequency at which the train arrives
+	// and the current time
+	// return the arrival time of the next train
+	// For example:
+	// If the first train comes at 1:00 PM
+	// and the frequency is 30 minutes
+	// and it is currently 4:04 PM
+	// then I should return 4:30 PM
+	var now = moment();
 
-  $("#addTrainBtn").on("click", function(e) {
-      e.preventDefault();
+	// Currently returns a JavaScript date
+	return now.toDate();
+}
 
-      //declare variable for train input fields
-      var trainName = $("#trainNameInput").val();
-      var lineName = $("#lineInput").val();
-      var trainDestination = $("#destinationInput").val();
-      var trainFrequency = $("#frequencyInput").val();
+function getMinutesUntilNextArrival (nextTrainTime) {
+	// return the time difference 
+	// between next arrival and right now
+	var now = moment();
 
-      database.ref().push({ //database refererence for firebase and pushes the data to the DOM
+	return 100;
+}
 
-        dbTrainName: trainName,
-        dbLine: lineName,
-        dbDestination: trainDestination,
-        dbFrequency: trainFrequency
-      })
+//adding click handler function to submit the data
+$("#addTrainBtn").on("click", function(e) {
+		e.preventDefault();
 
-      $("#trainNameInput").val("");
-      $("#lineInput").val("");
-      $("#destinationInput").val("");
-      $("#frequencyInput").val("");
+		//declare variable for train input fields
+		var trainName = $("#trainNameInput").val();
+		var lineName = $("#lineInput").val();
+		var trainDestination = $("#destinationInput").val();
+		var trainFrequency = $("#frequencyInput").val();
+		var firstTrainTime = $('#trainTimeInput').val();
 
+		database.ref().push({ //database refererence for firebase and pushes the data to the DOM
+			dbTrainName: trainName,
+			dbLine: lineName,
+			dbDestination: trainDestination,
+			dbFrequency: trainFrequency,
+			dbTrainTime: firstTrainTime
+		})
 
-  })
-      //function that adds the data to firebase database
-    database.ref().on("child_added", function (snapshot) {
-      console.log(snapshot.val());
+		$("#trainNameInput").val("");
+		$("#lineInput").val("");
+		$("#destinationInput").val("");
+		$("#frequencyInput").val("");
+		// TODO: clear train time too
+});
 
-      var trainLine;//variable to retrieve the data in the line field
-      
-      if ( snapshot.val().dbLine ==null)
-      {
-        trainLine= "";
-      }
-      else
-      {
-        trainLine = snapshot.val().dbLine;
-      }
+//function that adds the data to firebase database
+database.ref().on("child_added", function (snapshot) {
+	console.log(snapshot.val());
 
-      
+	var trainLine;//variable to retrieve the data in the line field
+	
+	if ( snapshot.val().dbLine ==null)
+	{
+		trainLine= "";
+	}
+	else
+	{
+		trainLine = snapshot.val().dbLine;
+	}
 
-      $("#trainTable").append("<tr>" + 
-        "<td>" + snapshot.val().dbTrainName + "</td>" +
-        "<td>" + trainLine + "</td>" +
-         "<td>" + snapshot.val().dbDestination + "</td>" + "<td>" + snapshot.val().dbFrequency + "</td> </tr>");
-        
+	
+	var nextArrival = getNextArrival(snapshot.val().dbTrainTime);
 
-    });
+	$("#trainTable").append("<tr>" + 
+		"<td>" + snapshot.val().dbTrainName + "</td>" +
+		"<td>" + trainLine + "</td>" +
+		"<td>" + snapshot.val().dbDestination + "</td>" + 
+		"<td>" + snapshot.val().dbFrequency + "</td>" +
+		"<td>" +  moment(nextArrival).format('LTS') + "</td>" +
+		"<td>" + getMinutesUntilNextArrival(nextArrival) + "</td>" +
+	" </tr>");
+});
 
-    
